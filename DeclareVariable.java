521265ab7cdc18f56a5c970325db2be206405430
Vishal yadav <vishalyadavpbh2005@gmail.com>
2026-02-14 19:23:01 +0530

    Add declare variable pattern

---
+++ DeclareVariable.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class DeclareVariable {
+    public static void main(String[] args) {
+        
+        int age = 21;                     // Age → whole number
+        float salary = 45000.50f;          // Salary → decimal value
+        char grade = 'A';                  // Grade → single character
+        String name = "Vishal";            // Name → text
+        boolean isStudent = true;          // True / False value
+        double temperature = 36.6;         // Precise decimal value
+        String mobileNumber = "9335084156"; // Stored as text
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Mobile Number: " + mobileNumber);
+    }
+    }
