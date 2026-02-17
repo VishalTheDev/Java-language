816d6631d1d9e9dead0c03c12273dc86267df99c
Vishal yadav <vishalyadavpbh2005@gmail.com>
2026-02-17 21:50:24 +0530

    Add array pattern

---
+++ Array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 97; //physics
+        marks[1] = 98; //chemistry
+        marks[2] = 95; //english
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        for(int i=0; i<3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
--- FactorilNumber.java
+++ FactorilNumber.java
@@ -20,6 +20,6 @@ public class FactorilNumber {
         printFactorialNumber(n);
 
         sc.close();
-    }
+    } 
     
 }
